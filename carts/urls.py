dd7043392bf1a10a47e1770f079f9041369cd3ff
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-08 09:55:40 +0100

    cart app and cart design

---
+++ carts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import cart
+
+
+urlpatterns = [
+    path('', cart, name='cart'),
+
+]
--- imani/settings.py
+++ imani/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'djmoney',
+    'carts',
 ]
 
 MIDDLEWARE = [
