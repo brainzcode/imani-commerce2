598fa9d423f693944bea9d34f59b4965d5531e37
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-08 11:04:44 +0100

    functionality without logging in

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import cart
+from .views import cart, add_cart
 
 
 urlpatterns = [
     path('', cart, name='cart'),
-
+    path('add_cart/<int:product_id>', add_cart, name='add_cart'),
 ]
