9c97f9a3791d7d5e27127019e5649e0e3e3348ef
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-15 01:14:38 +0100

    paginator Initiated

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
@@ -13,12 +16,18 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 9)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 9)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
         
-    context = {'products': products, 'product_count': product_count}
+    context = {'products': paged_products, 'product_count': product_count}
     return render(request, 'store/store.html', context)
 
 
