766bbc0f39f1ca0568e88397ee7845fa9f008c6d
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-02-17 10:22:04 +0100

    Instance Variation 1

--- carts/views.py
+++ carts/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
 
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
+
 
 
 # Create your views here.
@@ -18,9 +19,15 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-        print(color, size)
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(variation_category__iexact=key, variation_value__iexact=value)
+                print(variation)
+            except:
+                pass
 
     
     product = Product.objects.get(id=product_id)
