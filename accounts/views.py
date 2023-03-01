f92e1f5ff12673cc5a4d5cf0e70cd3246428c1f6
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-01 02:09:18 +0100

    registration implementation

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .forms import RegisterationForm
+from .forms import RegistrationForm
 
 # Create your views here.
 
 def register(request):
-  form = RegisterationForm()
+  form = RegistrationForm()
   context = {'form': form}
   return render(request, 'accounts/register.html', context)
 
