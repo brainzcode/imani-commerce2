2c20ad0656200ec133b5e8e9529c94bf3c355881
brainzcode <98150590+brainzcode@users.noreply.github.com>
2024-07-02 15:31:45 -0400

    home page updated

--- imani/settings.py
+++ imani/settings.py
@@ -128,19 +128,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
-
-
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'imani/static',
+    BASE_DIR / 'static',
 ]
+
+
+
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     'imani/static',
+# ]
 
 # Media files config
 MEDIA_URL = '/media/'
